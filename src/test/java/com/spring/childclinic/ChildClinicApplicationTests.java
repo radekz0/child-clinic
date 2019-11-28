b935ea331605eb7e0ea513452b89e9a1c9386bee
radekz0 <r.zielinski1221@gmail.com>
2019-11-28 21:41:59 +0100

    Initial commit

---
+++ src/test/java/com/spring/childclinic/ChildClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.childclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
