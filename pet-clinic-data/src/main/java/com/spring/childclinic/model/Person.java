0011ecbe0a6b427d8119763db407699c68cece4b
radekz0 <r.zielinski1221@gmail.com>
2019-11-29 11:28:30 +0100

    BaseEntity added.

---
+++ pet-clinic-data/src/main/java/com/spring/childclinic/model/Person.java
@@ -0,0 +1,32 @@
+package com.spring.childclinic.model;
+
+public class Person extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
