30e5deca4fddd278089f04aab50b12d5b599ca14
radekz0 <r.zielinski1221@gmail.com>
2019-12-12 13:02:34 +0100

    Component scan fix.

--- child-clinic-web/src/main/java/com/spring/childclinic/ChildClinicApplication.java
+++ child-clinic-web/src/main/java/com/spring/childclinic/ChildClinicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"child-clinic-data", "child-clinic-web"})
+@ComponentScan(basePackages ="com.spring.childclinic")
 public class ChildClinicApplication {
 
 	public static void main(String[] args) {
--- child-clinic-web/src/main/java/com/spring/childclinic/controllers/ParentController.java
+++ child-clinic-web/src/main/java/com/spring/childclinic/controllers/ParentController.java
@@ -1,23 +1,30 @@
 package com.spring.childclinic.controllers;
 
 import com.spring.childclinic.service.ParentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RequestMapping("/parents")
 @Controller
 public class ParentController {
 
+    Logger logger = LoggerFactory.getLogger(ParentController.class);
     private final ParentService parentService;
 
     public ParentController(ParentService parentService) {
+        logger.info("logger Inside controller constructor-----------------------------------------------------------------------");
+        logger.debug("debugging the controller constructor");
+        System.out.println("Inside controller constructor---------------------------------------------------------------");
         this.parentService = parentService;
     }
 
-    @RequestMapping({"", "/"})
+    @GetMapping
     public String listParents(Model model){
-        System.out.println("inside parent controller");
         model.addAttribute("parents", parentService.findAll());
         System.out.println("inside parent controller");
         return "parents/index";
