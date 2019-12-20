b8279de0e0eda6baa5c300306d6b62d89ac8e86e
radekz0 <r.zielinski1221@gmail.com>
2019-12-20 14:52:38 +0100

    Main layout added.

--- child-clinic-data/src/main/java/com/spring/childclinic/model/Parent.java
+++ child-clinic-data/src/main/java/com/spring/childclinic/model/Parent.java
@@ -1,4 +1,15 @@
 package com.spring.childclinic.model;
 
+import java.util.Set;
+
 public class Parent extends Person {
+    public Set<Child> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Set<Child> children) {
+        this.children = children;
+    }
+
+    private Set<Child> children;
 }
