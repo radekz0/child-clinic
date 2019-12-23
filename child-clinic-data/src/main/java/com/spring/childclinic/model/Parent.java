0882cb3f1df3bc9da9229ce80be8b407aa3f0ac1
radekz0 <r.zielinski1221@gmail.com>
2019-12-23 13:46:41 +0100

    Parent entity extended.

--- child-clinic-data/src/main/java/com/spring/childclinic/model/Parent.java
+++ child-clinic-data/src/main/java/com/spring/childclinic/model/Parent.java
@@ -3,6 +3,36 @@ package com.spring.childclinic.model;
 import java.util.Set;
 
 public class Parent extends Person {
+
+    private String address;
+    private String city;
+    private String telephonne;
+    private Set<Child> children;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephonne() {
+        return telephonne;
+    }
+
+    public void setTelephonne(String telephonne) {
+        this.telephonne = telephonne;
+    }
+
     public Set<Child> getChildren() {
         return children;
     }
@@ -10,6 +40,4 @@ public class Parent extends Person {
     public void setChildren(Set<Child> children) {
         this.children = children;
     }
-
-    private Set<Child> children;
 }
